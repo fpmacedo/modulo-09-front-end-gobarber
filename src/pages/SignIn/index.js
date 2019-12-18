92bb0fac73b01012a8df2d97c4f90e8c58414b9f
fpmacedo <lipe_macedo@msn.com>
2019-12-18 10:56:29 -0300

    Finalizada aula de estilos globais

---
+++ src/pages/SignIn/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import AuthLayout from '../_layout/auth';
+
+// import { Container } from './styles';
+
+export default function SignUp() {
+  return <AuthLayout>Login</AuthLayout>;
+}
