4bc0b9b0f6d279fa88a35bc4803620f05a74c95f
fpmacedo <lipe_macedo@msn.com>
2019-12-18 17:53:45 -0300

    Finalizada aula de autenticacao

--- src/store/modules/auth/sagas.js
+++ src/store/modules/auth/sagas.js
@@ -1,3 +1,25 @@
-import { all } from 'redux-saga/effects';
+import { all, takeLatest, call, put } from 'redux-saga/effects';
 
-export default all([]);
+import history from '~/services/history';
+import api from '~/services/api';
+
+import { singInSuccess } from './actions';
+
+export function* signIn({ payload }) {
+  const { email, password } = payload;
+
+  const response = yield call(api.post, 'sessions', { email, password });
+
+  const { token, user } = response.data;
+
+  // console.tron.log(response.data);
+
+  if (!user.provider) {
+    console.tron.error('Usuario nao e prestador');
+  }
+
+  yield put(singInSuccess(token, user));
+
+  history.push('/dashboard');
+}
+export default all([takeLatest('@auth/SIGN_IN_REQUEST', signIn)]);
