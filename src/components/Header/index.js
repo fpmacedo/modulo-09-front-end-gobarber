a70eed2d075f011886ce18039e8642989c87fdc4
fpmacedo <lipe_macedo@msn.com>
2019-12-19 18:15:22 -0300

    Finalizada aula de notificacoes

---
+++ src/components/Header/index.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import Notifications from '~/components/Notifications';
+import logo from '~/assets/logo-purple.svg';
+import { Container, Content, Profile } from './styles';
+
+export default function Header() {
+  return (
+    <Container>
+      <Content>
+        <nav>
+          <img src={logo} alt="gobarber" />
+          <Link to="/dashboard">Dashboard</Link>
+        </nav>
+
+        <aside>
+          <Notifications />
+          <Profile>
+            <div>
+              <strong>Filipe Macedo</strong>
+              <Link to="/profile">Meu perfil</Link>
+            </div>
+            <img
+              src="https://api.adorable.io/avatars/50/abott@adorable.png"
+              alt="Filipe Macedo"
+            />
+          </Profile>
+        </aside>
+      </Content>
+    </Container>
+  );
+}
