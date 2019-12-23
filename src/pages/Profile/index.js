8a2f21986860f2c2f3273c8423cc08dac206643f
fpmacedo <lipe_macedo@msn.com>
2019-12-23 11:56:51 -0300

    Finalizado o Gobarber Web

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -2,7 +2,10 @@ import React from 'react';
 import { Form, Input } from '@rocketseat/unform';
 import { useSelector, useDispatch } from 'react-redux';
 
+import { signOut } from '~/store/modules/auth/actions';
 import { updateProfileRequest } from '~/store/modules/user/actions';
+
+import AvatarInput from './AvatarInput';
 
 import { Container } from './styles';
 
@@ -14,9 +17,13 @@ export default function SignUp() {
     dispatch(updateProfileRequest(data));
   }
 
+  function handleSignOut() {
+    dispatch(signOut());
+  }
   return (
     <Container>
       <Form initialData={profile} onSubmit={handleSubmit}>
+        <AvatarInput name="avatar_id" />
         <Input name="name" placeholder="Nome completo" />
         <Input name="email" placeholder="Seu endereco de e-mail" />
         <hr />
@@ -29,7 +36,9 @@ export default function SignUp() {
         />
         <button type="submit">Atualizar perfil</button>
       </Form>
-      <button type="button">Sair do Gobarber</button>
+      <button type="button" onClick={handleSignOut}>
+        Sair do Gobarber
+      </button>
     </Container>
   );
 }
